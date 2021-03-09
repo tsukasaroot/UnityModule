72e9cdc7a6777af10b43835ab14e85acdf93f3c7
Arthur Barbier <Arthur Barbier>
2021-03-09 14:56:21 +0100

    add player

---
+++ Assets/scripts/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody rb;
+    public Transform camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float v = Input.GetAxis("Vertical");
+
+        rb.AddForce(transform.forward * v * speed);
+        rb.MoveRotation(camera.rotation);
+    }
+}
