da4c70d1303656d6d032c6b7da50f16a7b633051
Brian Merlin <brian.merlin@epitech.eu>
2021-03-23 15:12:24 +0100

    Add trophy + Rotate script

---
+++ Assets/TrophyCups/Prefabs/Gold/RotateTrophy.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateTrophy : MonoBehaviour
+{
+
+    public Vector3 RotateAmount;
+
+    void Start()
+    {
+        StartCoroutine("RotationTrophy");
+    }
+
+    void Update()
+    {
+
+    }
+
+    IEnumerator RotationTrophy()
+    {
+        while (true)
+        {
+            transform.Rotate(RotateAmount * Time.deltaTime);
+            yield return null;
+        }
+    }
+}
