48c835a8ae57c73ab32f07dbc58657ba6009d944
Petshop69 <37632963+User455@users.noreply.github.com>
2021-03-23 15:57:43 +0100

    Update camera.cs
    add x axis for camera

--- Assets/scripts/camera.cs
+++ Assets/scripts/camera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class camera : MonoBehaviour
 {
     public float sensitivity = 10.0f;
-    public float upSensitivity = 5.0f;
+    public float upSensitivity = 3.0f;
     public float distance = 5f;
     public Transform targetToFollow;
     // Start is called before the first frame update
@@ -17,22 +17,27 @@ public class camera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float yRotation = (Input.GetAxis("Mouse X") * sensitivity);
-        float xRotation = (Input.GetAxis("Mouse Y") * upSensitivity);
-        //float yRotation = transform.rotation.y + (Input.GetAxis("Mouse X") * sensitivity);
-        //if (yRotation > 1) yRotation -= 2;
-        //if (yRotation < -1) yRotation += 2;
-        //transform.rotation = new Quaternion(transform.rotation.x, yRotation, transform.rotation.z, transform.rotation.w);
+        float xRotation = transform.rotation.eulerAngles.x + (Input.GetAxis("Mouse Y") * upSensitivity);
+        float yRotation = transform.rotation.eulerAngles.y + (Input.GetAxis("Mouse X") * sensitivity);
+        float zRotation = transform.rotation.eulerAngles.z;
 
-        transform.Rotate(0.0f, yRotation, 0.0f, Space.Self);
-        //transform.Rotate(xRotation, 0.0f, 0.0f, Space.World);
-        //transform.Rotate(((yRotation > 0) ? Vector3.up : Vector3.down) * yRotation * Time.deltaTime);
+        if (xRotation > 30 && xRotation < 330)
+        {
+            if (transform.rotation.eulerAngles.x <= 40)
+            {
+                xRotation = 30;
+            } else
+            {
+                xRotation = 330;
+            }
+        }
+        transform.eulerAngles = new Vector3(xRotation, yRotation, zRotation);
         float angle = transform.rotation.eulerAngles.y;
         if (angle < 0)
             angle += 360;
         transform.position = new Vector3(
             targetToFollow.position.x - (distance * Mathf.Sin(Mathf.PI * 2 * angle / 360)),
-            transform.position.y,
+            targetToFollow.position.y + (((xRotation < 180) ? xRotation : xRotation - 360) / 10),
             targetToFollow.position.z - (distance * Mathf.Cos(Mathf.PI * 2 * angle / 360))
         );
     }
