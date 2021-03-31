4a3ad537951e1f575d4bd5251332b1b4161ef050
zelioh <38160465+zelioh@users.noreply.github.com>
2021-03-31 16:07:24 +0200

    Added cursor visibility when the player is destroy

--- Assets/scripts/player.cs
+++ Assets/scripts/player.cs
@@ -137,6 +137,11 @@ public class player : MonoBehaviour
         }
     }
 
+    private void OnDestroy()
+    {
+        Cursor.visible = true;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "DeathZone" || collision.collider.name.Contains("TunnelEnding"))
